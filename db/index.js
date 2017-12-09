c037d182e67ff53054dce34566ad80d72ed9e140
Ali <ali.alavi201@gmail.com>
2017-12-09 00:38:03 -0800

    ignore

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
 const pgp = require('pg-promise')();
-const connection = pgp( process.env.DATABASE_URL || 'postgres://postgres@localhost:5432/scrabble' );
+const connection = pgp( process.env.DATABASE_URL || 'postgres://postgres:123456@localhost:5432/ALIEXPRESSAPP' );
 
 module.exports = connection;
