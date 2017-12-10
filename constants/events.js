8165d79c7de82f43565a263aea017e575fa3903f
ajinkyachalke <achalke@mail.sfsu.edu>
2017-12-09 18:48:52 -0800

    events constant from the previoud merge

--- constants/events.js
+++ constants/events.js
@@ -4,7 +4,6 @@ const CONNECTION = 'connection';
 const DISCONNECT = 'disconnect';
 const INVALID_MOVE = 'invalid move';
 const NO_DATA = 'no data';
-<<<<<<< HEAD
 const RACK = 'rack';
 const CHAT_RECEIVED = 'server chat sent';
 
@@ -12,9 +11,3 @@ module.exports = { CHAT_MESSAGE, TILE, CONNECTION, DISCONNECT, INVALID_MOVE, NO_
 
 
 
-=======
-const CHAT_RECEIVED = 'server chat sent';
-const RACK = 'rack';
-
-module.exports = { CHAT_MESSAGE, TILE, CONNECTION, DISCONNECT, INVALID_MOVE, NO_DATA, RACK,CHAT_RECEIVED };
->>>>>>> f9f5709fff683fca2a4bccc63f155c4536541273
